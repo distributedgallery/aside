7485da4e0148c4683ac0a910c0f4d292b1ed7411
Olivier Sarrouy <me@osarrouy.bzh>
2024-02-25 16:45:31 +0100

    Fix CORS issue

--- api/aisentiment/index.js
+++ api/aisentiment/index.js
@@ -2,6 +2,7 @@ import aisentiment from '../../src/lib/aisentiment.js';
 
 export default async function handler(request, response) {
 	try {
+		response.setHeader('Access-Control-Allow-Origin', '*');
 		return response.status(200).json({ sentiment: (await aisentiment.last()).sentiment });
 	} catch (error) {
 		console.log(error);
