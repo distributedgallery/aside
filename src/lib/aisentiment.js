faba42e57cbe7d067182f18f48b388fb8974381b
Olivier Sarrouy <me@osarrouy.bzh>
2024-03-04 10:18:22 +0100

    Clean lib

--- src/lib/aisentiment.js
+++ src/lib/aisentiment.js
@@ -1,40 +1,19 @@
-import { createClient } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 
 const aisentiments = {
 	PAGE_SIZE: 24,
 	last: async () => {
-		const client = createClient();
-		await client.connect();
-
-		try {
-			const { rows } = await client.sql`SELECT * FROM Sentiments ORDER BY timestamp DESC LIMIT 1;`;
-			return rows[0];
-		} finally {
-			await client.end();
-		}
+		const { rows } = await sql`SELECT * FROM Sentiments ORDER BY timestamp DESC LIMIT 1;`;
+		return rows[0];
 	},
 	page: async (page = 1) => {
-		const client = createClient();
-		await client.connect();
-
-		try {
-			const { rows } =
-				await client.sql`SELECT * FROM Sentiments ORDER BY timestamp DESC OFFSET ${(page - 1) * aisentiments.PAGE_SIZE} LIMIT ${aisentiments.PAGE_SIZE}`;
-			return rows;
-		} finally {
-			await client.end();
-		}
+		const { rows } =
+			await sql`SELECT * FROM Sentiments ORDER BY timestamp DESC OFFSET ${(page - 1) * aisentiments.PAGE_SIZE} LIMIT ${aisentiments.PAGE_SIZE}`;
+		return rows;
 	},
 	count: async () => {
-		const client = createClient();
-		await client.connect();
-
-		try {
-			const { rows } = await client.sql`SELECT COUNT(*) FROM Sentiments;`;
-			return rows[0].count;
-		} finally {
-			await client.end();
-		}
+		const { rows } = await sql`SELECT COUNT(*) FROM Sentiments;`;
+		return rows[0].count;
 	}
 };
 
