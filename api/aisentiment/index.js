4ba1d57217d7fae0cc2e86cd466e55f3a1a8b3b9
Olivier Sarrouy <me@osarrouy.bzh>
2024-03-04 13:17:18 +0100

    Try to fix timeout issue

--- api/aisentiment/index.js
+++ api/aisentiment/index.js
@@ -1,12 +1,12 @@
 import aisentiment from '../../src/lib/aisentiment.js';
 
+export const maxDuration = 200;
 export const dynamic = 'force-dynamic';
 
 export default async function handler(request, response) {
 	try {
 		console.log(request);
 		response
-			// .setHeader('Access-Control-Allow-Credentials', true)
 			.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
 			.setHeader('Access-Control-Allow-Origin', '*');
 		return response.status(200).json({ sentiment: (await aisentiment.last()).sentiment });
