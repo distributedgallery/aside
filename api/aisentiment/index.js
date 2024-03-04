3c69b1d6a689896a38d07d15826ed3cd4dc02aa9
Olivier Sarrouy <me@osarrouy.bzh>
2024-03-04 12:51:32 +0100

    Still try to fix 304 on API calls

--- api/aisentiment/index.js
+++ api/aisentiment/index.js
@@ -6,6 +6,7 @@ export default async function handler(request, response) {
 	try {
 		response
 			// .setHeader('Access-Control-Allow-Credentials', true)
+			.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
 			.setHeader('Access-Control-Allow-Origin', '*');
 		return response.status(200).json({ sentiment: (await aisentiment.last()).sentiment });
 	} catch (error) {
